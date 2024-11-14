fc0b8033ffc27e1fbf3608036ac0e807fc76da9f
WithoutSFz <163065229+WithoutSFz@users.noreply.github.com>
2024-11-14 12:01:39 -0300

    Create ElemVenta.java

---
+++ ElemVenta.java
@@ -0,0 +1,9 @@
+public  abstract class ElemVenta{
+  private String nombre;//get y set
+
+  public abstract double getPrecio();
+  public abstract dpuble getVolumen();
+  public abstract String getMarca();
+  
+  
+}
