50ef41c666262945ffcff3e2ce6ad4408a75b455
WithoutSFz <163065229+WithoutSFz@users.noreply.github.com>
2024-11-14 11:28:23 -0300

    Create Producto.java

---
+++ Producto.java
@@ -0,0 +1,15 @@
+Public class Producto{
+  private String nombre;
+  private String marca;
+  private Double precio;
+  private Double volumen;
+  //get y set de todo
+  public Producto (String n,m, Double p,v){
+    nombre=n;
+    marca=m;
+    precio=p;
+    volumen=v;
+  }
+
+  
+}
