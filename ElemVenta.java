800162a4ab2311d07c8362b1e49919dd7839267f
WithoutSFz <163065229+WithoutSFz@users.noreply.github.com>
2024-11-14 12:02:39 -0300

    Update ElemVenta.java

--- ElemVenta.java
+++ ElemVenta.java
@@ -1,6 +1,8 @@
 public  abstract class ElemVenta{
   private String nombre;//get y set
-
+   public ElemVenta(String n){
+     nombre=n;
+   }
   public abstract double getPrecio();
   public abstract dpuble getVolumen();
   public abstract String getMarca();
